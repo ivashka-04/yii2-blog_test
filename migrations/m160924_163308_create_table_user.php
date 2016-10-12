e10ffe1362f3172e2ac287562b99656fd1ef27a8
Ivashka-04 <iskatel04@gmail.com>
2016-10-13 01:32:13 +0600

    дописан user файл как в уроке

---
+++ migrations/m160924_163308_create_table_user.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+class m160924_163308_create_table_user extends Migration
+{
+    /*public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160924_163308_create_table_user cannot be reverted.\n";
+
+        return false;
+    }*/
+
+
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+        $this->execute("
+        
+        ");
+    }
+
+    public function safeDown()
+    {
+    }
+
+}
